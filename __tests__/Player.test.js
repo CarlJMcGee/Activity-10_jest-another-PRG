f6c18cce98b1e35fb0687cdc24b55724999e84a7
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-05-04 16:44:38 -0500

    added getStat and getInventory methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -8,9 +8,28 @@ test("creates player obj", () => {
 
   expect(player.name).toBe("Dan");
   expect(player.health).toEqual(expect.any(Number));
-  expect(player.strenth).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
+});
+
+test("gets player stats", () => {
+  const player = new Player("Ben");
+
+  expect(player.getStats()).toHaveProperty("potions");
+  expect(player.getStats()).toHaveProperty("health");
+  expect(player.getStats()).toHaveProperty("strength");
+  expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("gets player inventory", () => {
+  const player = new Player("Andrew");
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
--- lib/Player.ts
+++ lib/Player.ts
@@ -5,10 +5,23 @@ function Player(name = "") {
   this.name = name;
 
   this.health = playerNumGen(105, 95);
-  this.strenth = playerNumGen(12, 7);
+  this.strength = playerNumGen(12, 7);
   this.agility = playerNumGen(12, 7);
 
   this.inventory = [new playerPotion("health"), new playerPotion()];
 }
+
+Player.prototype.getStats = function () {
+  return {
+    potions: this.inventory.length,
+    health: this.health,
+    strength: this.strength,
+    agility: this.agility,
+  };
+};
+
+Player.prototype.getInventory = function () {
+  return this.inventory.length ? this.inventory : false;
+};
 
 module.exports = Player;
