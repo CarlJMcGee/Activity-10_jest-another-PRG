b9a006c189641486a8de021bf6c9fa5b1afedc0d
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-05-04 13:39:03 -0500

    Created Potion constructor

---
+++ lib/Potion.ts
@@ -0,0 +1,15 @@
+const randomNumGen = (high: number, low: number) =>
+  Math.floor(Math.random() * (high - low) + low);
+
+function Potion(name: string) {
+  this.types = ["strenth", "agility", "health"];
+  this.name = name || this.types[randomNumGen(this.types.length, 1)];
+
+  //   if (this.name === "health") {
+  //     this.value = randomNumGen(40, 30);
+  //   }
+
+  this.value = name === "health" ? randomNumGen(40, 30) : randomNumGen(12, 7);
+}
+
+module.exports = Potion;
